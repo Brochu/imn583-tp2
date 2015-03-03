c083cb7dcf73830fd50f89ab3de306c7d84f1ef4
Brochu <alexbrochu1@gmail.com>
2015-03-03 14:29:14 -0500

    Interpolation par CatmullRom fonctionnelle aww yiss

--- TP2/Source/Curves/CatmullRomInterp.cpp
+++ TP2/Source/Curves/CatmullRomInterp.cpp
@@ -1,15 +1,5 @@
-#include <iostream>
 #include "CatmullRomInterp.h"
 
-
-CatmullRomInterp::CatmullRomInterp()
-{
-    // Creation de la matrice de Catmull Rom
-    M = Matrix4(-1.0, 3.0, -3.0, 1.0,
-        2.0, -5.0, 4.0, -1.0,
-        -1.0, 0.0, 1.0, 0.0,
-        0.0, 2.0, 0.0, 0.0);
-}
 void CatmullRomInterp::GetPointTangeante(float aProgression, Vector3 &aPoint, Vector3 &aTangeante)
 {
     //On clamp pour être sûr que la progression est entre 0 et 1.
@@ -19,17 +9,33 @@ void CatmullRomInterp::GetPointTangeante(float aProgression, Vector3 &aPoint, Ve
     int PointMin = (int)(aProgression*(mto_CtrlPoints.size() - 1));
 
     // On trouve les pts necessaire au calcul
-    int Pm1 = (PointMin != 0) ? (PointMin - 1) : (mto_CtrlPoints.size() - 1);
-    int Pp1 = (PointMin + 1) % mto_CtrlPoints.size();
-    int Pp2 = (PointMin + 2) % mto_CtrlPoints.size();
+    int Pm1 = PointMin - 1;
+    if (Pm1 < 0) Pm1 = (mto_CtrlPoints.size() - 2);
 
+    int Pp1 = (PointMin + 1) % (mto_CtrlPoints.size() - 1);
+    int Pp2 = (PointMin + 2) % (mto_CtrlPoints.size() - 1);
+
+    // Calcul de la progression courante
     float u = (aProgression * (mto_CtrlPoints.size() - 1)) - PointMin;
 
-    // Calculer P(u)
-    Matrix4 B(mto_CtrlPoints[Pm1].X(), mto_CtrlPoints[Pm1].Y(), mto_CtrlPoints[Pm1].Z(), 0.0,
-        mto_CtrlPoints[PointMin].X(), mto_CtrlPoints[PointMin].Y(), mto_CtrlPoints[PointMin].Z(), 0.0,
-        mto_CtrlPoints[Pp1].X(), mto_CtrlPoints[Pp1].Y(), mto_CtrlPoints[Pp1].Z(), 0.0,
-        mto_CtrlPoints[Pp2].X(), mto_CtrlPoints[Pp2].Y(), mto_CtrlPoints[Pp2].Z(), 0.0);
+    // On obtient l'information complete sur les pts necessaires
+    Vector3 p0 = mto_CtrlPoints[Pm1];
+    Vector3 p1 = mto_CtrlPoints[PointMin];
+    Vector3 p2 = mto_CtrlPoints[Pp1];
+    Vector3 p3 = mto_CtrlPoints[Pp2];
 
-    // see http://www.mvps.org/directx/articles/catmull/ pour le calcul de P(u)
+    // Voir http://www.mvps.org/directx/articles/catmull/ pour le calcul de P(u)
+    Vector3 a = (-1 * p0 + 3 * p1 - 3 * p2 + p3) * powf(u, 3);
+    Vector3 b = (2 * p0 - 5 * p1 + 4 * p2 - p3) * powf(u, 2);
+    Vector3 c = (-1 * p0 + p2) * u;
+    Vector3 d = (2 * p1);
+
+    aPoint = 0.5 * (a + b + c + d);
+
+    // Calcul de la tangeante
+    Vector3 aprime = (-1 * p0 + 3 * p1 - 3 * p2 + p3) * (3 * powf(u, 2));
+    Vector3 bprime = (2 * p0 - 5 * p1 + 4 * p2 - p3) * (2 * u);
+    Vector3 cprime = (-1 * p0 + p2);
+
+    aTangeante = 0.5 * (aprime + bprime + cprime);
 }
--- TP2/Source/Curves/CatmullRomInterp.h
+++ TP2/Source/Curves/CatmullRomInterp.h
@@ -2,17 +2,11 @@
 #define CATMULLROMINTERP_H_
 
 #include "Curve.h"
-#include "../Foundation/Matrix4.h"
-#include "../Foundation/Vector3.h"
 
 class CatmullRomInterp : public Curve
 {
 public:
-    CatmullRomInterp();
     virtual void GetPointTangeante(float aProgression, Vector3 &aPoint, Vector3 &aTangeante);
-
-private:
-    Matrix4 M;
 };
 
 #endif //CATMULLROMINTERP_H_
